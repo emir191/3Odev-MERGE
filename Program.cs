f3e794f4d2e2610dee482088ce3e828c159db149
coders <hi@co>
2025-04-21 21:46:30 +0300

    program dosyası buradan silindi

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
-
-Console.WriteLine("Hello, World!");
-Dosya dosya=new Dosya();
-dosya.Awake();
-dosya.HarfSayisi("kelime");
-int harfAdeti=dosya.HarfSayisi("merhaba");
-Console.WriteLine("TOPLAM HARF"+harfAdeti);
